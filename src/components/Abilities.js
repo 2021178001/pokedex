48c216c8f3b228b6cc1b866fe6db168c2148cc2b
2021178001 <2021178001@uteq.edu.mx>
2021-04-09 19:52:52 -0500

    Agregado de Iterator en la parte de habilidades

--- src/components/Abilities.js
+++ src/components/Abilities.js
@@ -2,12 +2,37 @@ import React from 'react'
 
 function Abillities({abilities}) {
 
+    function Iterator(collection){
+        this.collection = collection;
+        this.index = 0;
+      }
+    
+      Iterator.prototype = {
+        hasNext: function(){
+          return this.index < this.collection.length
+        },
+        next: function(){
+          return this.collection[this.index++]
+        }
+      }
+
+      const abilitiesArray = [];
+      const iter = new Iterator(abilities);
+      
+      while(iter.hasNext()){
+          abilitiesArray.push(iter.collection[iter.index].ability.name);
+          iter.next()
+        }
+
     return (
         <section className="flex column abillities">
             <h1>Main skills</h1>
             <div>
-                <p>{abilities[0].ability.name}</p>
-                <p>{abilities[1].ability.name}</p>
+            {
+                   abilitiesArray.map((ability, index) => {
+                       return <p index={index}>{ability}</p>
+                   })
+               }
             </div>
         </section>
     )
